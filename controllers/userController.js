a9414b2925d1318ab098958806beada2a9d6699e
akcoolchamo <lamkhadeakshay98@gmail.com>
2024-08-24 11:25:53 +0530

    Restructured the codebase

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,21 @@
 import models from "../models/index.js";
-
+import {
+    sendErrorResponse,
+    sendSuccessResponse,
+    sendServerErrorResponse,
+} from "../utils/responseUtils.js";
+import { ERROR_MESSAGES, SUCCESS_MESSAGES } from "../utils/messages.js";
 export const createUser = async (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return sendErrorResponse(res, ERROR_MESSAGES.MISSING_NAME);
+    }
+
     try {
-        const user = await models.User.create(req.body);
-        res.status(200).json(user);
+        const user = await models.User.create({ name });
+        sendSuccessResponse(res, SUCCESS_MESSAGES.USER_CREATED, { user });
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        sendServerErrorResponse(res, ERROR_MESSAGES.SERVER_ERROR);
     }
 };
---
+++ utils/messages.js
@@ -0,0 +1,14 @@
+// Success Messages
+const SUCCESS_MESSAGES = {
+    USER_CREATED: "User created successfully.",
+};
+
+// Error Messages
+const ERROR_MESSAGES = {
+    INVALID_INPUT: "Invalid input.",
+    SERVER_ERROR: "Internal server error.",
+    MISSING_NAME: "Name is required.",
+};
+
+// Export messages
+export { SUCCESS_MESSAGES, ERROR_MESSAGES };
