9127d5d38f23b48d5706cbad84c9cd06d361e5b7
akcoolchamo <lamkhadeakshay98@gmail.com>
2024-08-24 11:44:15 +0530

    Get user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,18 +3,31 @@ import {
     sendErrorResponse,
     sendSuccessResponse,
     sendServerErrorResponse,
+    sendNotFoundResponse,
 } from "../utils/responseUtils.js";
 import { ERROR_MESSAGES, SUCCESS_MESSAGES } from "../utils/messages.js";
 export const createUser = async (req, res) => {
     const { name } = req.body;
-
     if (!name) {
         return sendErrorResponse(res, ERROR_MESSAGES.MISSING_NAME);
     }
-
     try {
         const user = await models.User.create({ name });
         sendSuccessResponse(res, SUCCESS_MESSAGES.USER_CREATED, { user });
+    } catch (error) {
+        sendServerErrorResponse(res, ERROR_MESSAGES.SERVER_ERROR);
+    }
+};
+
+export const getUser = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await models.User.findByPk(id);
+        if (!user) {
+            return sendNotFoundResponse(res, ERROR_MESSAGES.USER_NOT_FOUND);
+        }
+        sendSuccessResponse(res, SUCCESS_MESSAGES.USER_FETCHED, { user });
     } catch (error) {
         sendServerErrorResponse(res, ERROR_MESSAGES.SERVER_ERROR);
     }
--- utils/messages.js
+++ utils/messages.js
@@ -1,6 +1,7 @@
 // Success Messages
 const SUCCESS_MESSAGES = {
     USER_CREATED: "User created successfully.",
+    USER_FETCHED: "User fetched successfully.",
 };
 
 // Error Messages
@@ -8,6 +9,7 @@ const ERROR_MESSAGES = {
     INVALID_INPUT: "Invalid input.",
     SERVER_ERROR: "Internal server error.",
     MISSING_NAME: "Name is required.",
+    USER_NOT_FOUND: "User not found.",
 };
 
 // Export messages
